6faf2b095f37a72f6751cdd86e831011511d2de4
lcb42 <blatherwick.lucy@gmail.com>
2019-02-05 18:39:01 +0000

    Edited to collect both southampton and hants data

--- getDataDatafeed.py
+++ getDataDatafeed.py
@@ -18,19 +18,83 @@ password = 'NR0dpassword!'
 
 client = MongoClient()
 db = client["Third_Year_Project"]
-real_time_data = db["Real_Time_Data"]
+real_time_data = db["Real_Time_Data_1"]
+real_time_data_hants = db["Real_Time_Data_HANTS_1"]
+
+stanox_hants_stn_list = ["87031", "87021", "86074", "87763", "86066", "86896", "86339", "87024", "86216", "87009", "86202", "86921", "86061", "86901", "86223", "86108", "86915", "86301", "86112", "86107", "86087", "86343", "86241", "86042", "87010", "87026", "86045", "86321", "86077", "86222", "87062", "86341", "86201", "86332", "86913", "86049", "87064", "87065", "86908", "86907", "86081", "86527", "86219", "86911", "87012", "86070", "87066", "86917", "86248", "86313", "86311", "86703", "86101", "87067", "86122", "86084", "86218", "86495", "86520", "86499", "86225", "86909", "86497", "86711", "86342", "86071", "86083", "86047", "86215"]
+stanox_soton_stn_list = ['86520', '86216', '86527', '86703', '86499', '86218', '86495', '86497', '86215']
 
 def get_stanox(stanox):
 
     switcher = {
-        '86520': "SOU",
+        '87031': "AHT",
+        '87021': "AON",
+        '86074': "ADV",
+        '87763': "AHS",
+        '86066': "BSK",
+        '86896': "BEU",
+        '86339': "BDH",
+        '87024': "BTY",
         '86216': "BTE",
+        '87009': "BAW",
+        '86202': "BOE",
+        '86921': "BMH",
+        '86061': "BMY",
+        '86901': "BCU",
+        '86223': "BUO",
+        '86108': "CFR",
+        '86915': "CHR",
+        '86301': "CSA",
+        '86112': "DEN",
+        '86107': "DBG",
+        '86087': "ESL",
+        '86343': "EMS",
+        '86241': "FRM",
+        '86042': "FNB",
+        '87010': "FNN",
+        '87026': "FNH",
+        '86045': "FLE",
+        '86321': "FTN",
+        '86077': "GRT",
+        '86222': "HME",
+        '87062': "HSL",
+        '86341': "HAV",
+        '86201': "HDE",
+        '86332': "HLS",
+        '86913': "HNA",
+        '86049': "HOK",
+        '87064': "LIP",
+        '87065': "LIS",
+        '86908': "LYP",
+        '86907': "LYT",
+        '86081': "MIC",
         '86527': "MBK",
+        '86219': "NTL",
+        '86911': "NWM",
+        '87012': "NCM",
+        '86070': "OVR",
+        '87066': "PTR",
+        '86917': "POK",
+        '86248': "PTC",
+        '86313': "PMS",
+        '86311': "PMH",
         '86703': "RDB",
-        '86499': "SDN",
+        '86101': "ROM",
+        '87067': "RLN",
+        '86122': "SAL",
+        '86084': "SHW",
         '86218': "SHO",
         '86495': "SOA",
+        '86520': "SOU",
+        '86499': "SDN",
+        '86225': "SNW",
+        '86909': "SWY",
         '86497': "SWG",
+        '86711': "TTN",
+        '86342': "WBL",
+        '86071': "WCH",
+        '86083': "WIN",
+        '86047': "WNF",
         '86215': "WLS"
     }
 
@@ -52,13 +116,19 @@ class MyListener(object):
         stanox = data['body'].get('loc_stanox')
 
         # sou/bte/mbk/rdb/sdn/sho/soa/swg/wls
-        if (stanox == '86520') or (stanox == '86216') or (stanox == '86527') or (stanox == '86703') or (stanox == '86499') or (stanox == '86218') or (stanox == '86495') or (stanox == '86497') or (stanox == '86215'):
+        if stanox in stanox_soton_stn_list:
             print("\n**** ", get_stanox(stanox), "****\n")
             print(data)
             # Insert data into mongoDB
             x = real_time_data.insert_one(data)
             print("Inserted at: ", x.inserted_id)
 
+        if stanox in stanox_hants_stn_list:
+            print("\n**** ", get_stanox(stanox), "****\n")
+            print(data)
+            # Insert data into mongoDB
+            x = real_time_data_hants.insert_one(data)
+            print("Inserted at: ", x.inserted_id)
         #else:
         #    print("somewhere else ({})".format(stanox))
 
