a6c82ef8482a7757180e78e76f741fba5b83e83e
lcb42 <blatherwick.lucy@gmail.com>
2019-03-06 16:42:00 +0000

    Completed preprocessing and added RandomForestRegressor as the first thing to try. Successfully gotten output

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,13 @@
 from numpy.core.multiarray import ndarray
 from pymongo import MongoClient
 import numpy as np
-from scipy.sparse import csr_matrix
+import matplotlib.pyplot as plt
 import sklearn
+import time
+import datetime as dt
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
 
 # functions required
 def replace_space(element, char):
@@ -159,26 +164,6 @@ data = []
 change_orig_data = []
 cancel_data = []
 
-
-# for x in json_data:
-#     data.append([
-#        x['body']['event_type'],
-#        x['body']['planned_timestamp'],
-#        int(x['body']['timetable_variation']),  # Minutes variation from timetabled time
-#        # x['body']['original_loc_timestamp'],
-#        x['body']['current_train_id'],  # Indicates train has been reassigned -> does this have an impact on lateness
-#        int(replace_space(x['body']['next_report_run_time'], 0)),  # Likely minutes until next stanox report
-#        x['body']['actual_timestamp'],
-#        x['body']['correction_ind'],  # False if not a correction of a previous report, True if it is
-#        x['body']['platform'],
-#        x['body']['train_id'],
-#        x['body']['variation_status'],  # ON TIME, EARLY, LATE, OFF ROUTE
-#        x['body']['train_service_code'],
-#        x['body']['toc_id'],
-#        x['body']['loc_stanox'],
-#        x['body']['next_report_stanox']
-#    ])
-
 for x in json_change_orig_data:
     change_orig_data.append(list(x['body'].values()))
 
@@ -189,64 +174,147 @@ for x in json_cancel_data:
 # Change data to neg data as in characterisation
 
 neg_data = []
+label_data = []
 
 print("created array")
 
 for x in json_data:
-    neg_data.append([
-        x['body']['event_type'],                       # event_type
-        x['body']['planned_timestamp'],                       # planned timestamp
-        # x[3],                       # Current train id
-        # x[4],                       # likely mins until next stanox report WONT HAVE IN REAL INPUT DATA
-        # x[5],                       # timestamp of arrival/departure WONT HAVE IN REAL INPUT DATA
-        # x[6],                       # False if not a correction of a previous report, True if it is WONT HAVE IN REAL
-        # x[7],                       # Platform number WONT HAVE IN REAL INPUT DATA
-        x['body']['train_id'],                       # train_id
-        # x[9],                       # ONTIME/EARLY/LATE/OFFROUTE WONT HAVE IN REAL INPUT DATA
-        x['body']['train_service_code'],                      # train_service_code
-        x['body']['toc_id'],                      # toc_id
-        x['body']['loc_stanox'],                      # station stanox (location currently)
-        x['body']['next_report_stanox'],                      # the next station
-        check_status(int(x['body']['timetable_variation']), x['body']['variation_status'])    # Minutes variation from timetabled time LABEL TO FIND
-    ])
-
-_data = np.array(neg_data)
+    if x['body']['planned_timestamp'] != "":
+        neg_data.append([
+            x['body']['event_type'],                       # event_type
+            x['body']['planned_timestamp'],                       # planned timestamp
+            # x[3],                       # Current train id
+            # x[4],                       # likely mins until next stanox report WONT HAVE IN REAL INPUT DATA
+            # x[5],                       # timestamp of arrival/departure WONT HAVE IN REAL INPUT DATA
+            # x[6],                       # False if not a correction of a previous report, True if it is WONT HAVE IN REAL
+            # x[7],                       # Platform number WONT HAVE IN REAL INPUT DATA
+            # x['body']['train_id'],                       # train_id
+            # x[9],                       # ONTIME/EARLY/LATE/OFFROUTE WONT HAVE IN REAL INPUT DATA
+            x['body']['train_service_code'],                      # train_service_code
+            # x['body']['toc_id'],                      # toc_id
+            x['body']['loc_stanox']                      # station stanox (location currently)
+            # x['body']['next_report_stanox'],                      # the next station
+        ])
+        label_data.append(
+            int(check_status(int(x['body']['timetable_variation']), x['body']['variation_status']))
+            # Minutes variation from timetabled time LABEL TO FIND
+        )
 
 # Label/data
-X = _data[:, :6]
+X = np.array(neg_data)
 print(np.shape(X))
-y = _data[:, 7]
+y = label_data
 print(np.shape(y))
-
-
-# Split data into training/testing and validation 80-20, then split training/testing using k-fold cross validation
-from sklearn.model_selection import train_test_split
-X_train_test, X_valid, y_train_test, y_valid = train_test_split(X, y, test_size=0.2)
-
-X_train, X_test, y_train, y_test = train_test_split(X_train_test, y_train_test, test_size=0.2)
 
 
 # ********     Pre processing     ***********
 
-# One Hot encoding
-import pandas as pd
-
-df = pd.DataFrame(data=X_train, columns=["event_type", "planned_timestamp", "train_id", "train_service_code", "toc_id", "loc_stanox"])
+df = pd.DataFrame(data=X, columns=["event_type", "planned_timestamp", "train_service_code", "loc_stanox"])
 
 print(df.head(5))
+# Convert timestamp
 
-cat_cols = ['event_type', "train_id", "train_service_code", "toc_id", "loc_stanox"]
+years = []
+months = []
+days = []
+times = []
+weekdays = []
+
+for x in range(0, len(df)):
+    temp_time = int(int(df['planned_timestamp'][x]) / 1000)
+    readable = dt.datetime.fromtimestamp(temp_time).isoformat()
+
+    # years.append(int(readable.split("-")[0]))
+    # months.append(int(readable.split("-")[1]))
+    daytime = readable.split("-")[2]
+    days.append(daytime.split("T")[0])
+
+    hh = (daytime.split("T")[1]).split(":")[0]
+    mm = (daytime.split("T")[1]).split(":")[1]
+    ss = (daytime.split("T")[1]).split(":")[2]
+    times.append(int((dt.timedelta(hours=int(hh), minutes=int(mm), seconds=int(ss))).total_seconds()))
+
+    weekdays.append(dt.datetime.fromtimestamp(temp_time).weekday())
+
+# df['year'] = pd.Series(np.array(years)).values
+# df['month'] = pd.Series(np.array(months)).values
+df['day'] = pd.Series(np.array(days)).values
+df['time'] = pd.Series(np.array(times)).values
+df['weekday'] = pd.Series(np.array(weekdays)).values
+
+
+
+# One Hot Encoding using pandas
+cat_cols = ['event_type', "train_service_code", "loc_stanox"]
 df_processed = pd.get_dummies(df, prefix_sep="__", columns=cat_cols)
 
-print(df_processed.head(5))
+#print(df_p_X_train.head(1))
 
-
-
-# Convert timestamp
 
 # Previous Station -> delay at previous station?
 
-
 # Correlation between vars -> do after creating/hot encoding features
+# matrix = df_processed.corr()
+# sns.heatmap(matrix, xticklabels=1, yticklabels=1)
+# plt.show()
+
+# Split data into training/testing and validation 80-20, then split training/testing using k-fold cross validation
+from sklearn.model_selection import train_test_split
+X_train_test, X_valid, y_train_test, y_valid = train_test_split(df_processed, y, test_size=0.2)
+
+X_train, X_test, y_train, y_test = train_test_split(X_train_test, y_train_test, test_size=0.2)
 
 
+# Quick Random Forest
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+test_rmses = []
+train_rmses = []
+ns = [1,5,10,50,100,500,1000]
+repeats = 5
+
+for x in ns:
+
+    train_rmse_agg = 0
+    test_rmse_agg = 0
+
+    print(x)
+    for y in range(0, repeats):
+
+        regr = RandomForestRegressor(max_depth=5, n_estimators=x, n_jobs=-1)
+        # print("constructed")
+        regr.fit(X_train, y_train)
+        # print("trained")
+
+        # print(list(df_processed))
+        # print(regr.feature_importances_.tolist())
+
+        train_predict = regr.predict(X_train)
+        train_rmse = np.sqrt(((y_train - train_predict) ** 2).mean())
+        # print("Training RMSE: ", train_rmse)
+        train_rmse_agg += train_rmse
+
+        test_predict = regr.predict(X_test)
+        test_rmse = np.sqrt(((y_test - test_predict) ** 2).mean())
+        # print("Testing RMSE: ", test_rmse)
+        test_rmse_agg += test_rmse
+
+    train_rmses.append(train_rmse_agg/repeats)
+    test_rmses.append(test_rmse_agg/repeats)
+
+    # print(y_test)
+    # print(test_predict)
+
+
+plt.plot(ns, test_rmses, "b*", label="Test RMSE")
+plt.plot(ns, train_rmses, "r*", label="Train RMSE")
+plt.plot(ns, test_rmses, c='b')
+plt.plot(ns, train_rmses, c='r')
+plt.ylabel("RMSE Score")
+plt.xlabel("Variables of n_estimators")
+plt.xscale("log")
+plt.legend()
+plt.show()
